bd9e6f52ea94efd8940f61827f8a2c2b12a5f6a7
megarandolph <randolphmarte@hotmail.com>
2019-11-20 12:52:29 -0400

    Proyecto en blanco, primera fase para iniciar

---
+++ practicacinco.cero/practicacinco.cero/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(practicacinco.cero.Startup))]
+namespace practicacinco.cero
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
